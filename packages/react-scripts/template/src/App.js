9b78525be4d505803790c8fb9259624d7974589f
Ryan Hutton <hatnice42@gmail.com>
2018-11-10 15:09:18 -0600

    ensure successful netlify connection

--- packages/react-scripts/template/src/App.js
+++ packages/react-scripts/template/src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React - Testing - W/ CI
+            Learn React - Testing - W/ CI - Confirmed Working!!
           </a>
         </header>
       </div>
