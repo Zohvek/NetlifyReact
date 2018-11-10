ca00a596bf14762283835640b9d375d04ca8ea01
Ryan Hutton <hatnice42@gmail.com>
2018-11-10 15:52:43 -0600

    another one!

--- packages/react-scripts/template/src/App.js
+++ packages/react-scripts/template/src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React - Testing - W/ CI - Confirmed Working!!
+            Learn React - Testing - W/ CI - Confirmed Working!! I love John and John's Momma!!!
           </a>
         </header>
       </div>
